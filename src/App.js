1373276d72365352fd57afebacee3c4a422e1a61
arshad <mdarshadwarshi72@gmail.com>
2024-02-15 17:42:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Carditem from "./Component/CardItem/Carditem";
+
+function App() {
+  return (
+    <div className="App">
+      <Carditem />
+    </div>
+  );
+}
+
+export default App;
